a0eda938d362e5a7a7ca62bc57876fbed70e1ba1
Abdul-Hannan786 <abdulhannanhere@gmail.com>
2024-08-15 19:45:52 +0500

    revision

--- 2/app.js
+++ 2/app.js
@@ -3,65 +3,77 @@ const inputDiv = document.querySelector(".todo-list-container");
 
 let allTodos = [];
 
+window.onload = () => {
+  const savedTodos = JSON.parse(localStorage.getItem("todos"))
+  if(savedTodos){
+    allTodos = savedTodos
+    printAllTodos()
+  }
+}
+
+
 function createTodo() {
   let inputValue = input.value;
   allTodos.push(inputValue);
+  saveTodos()
   printAllTodos();
   input.value = "";
 }
 
 function printAllTodos() {
-  inputDiv.innerHTML = "";
-  allTodos.map(
-    (todo, index) =>
-      (inputDiv.innerHTML += `
+  let clutter = "";
+  allTodos.forEach((todo, index) => {
+    clutter += `
       <p>
-      ${todo}
+        ${todo}
         <button onclick="deleteTodo(${index})">Delete</button>
         <button onclick="editTodo(${index})">Edit</button>
       </p>
-    `)
-  );
+    `;
+  });
+
+  inputDiv.innerHTML = clutter;
 }
 
 function deleteTodo(index) {
   allTodos.splice(index, 1);
+  saveTodos()
   printAllTodos();
 }
 
 const addContainer = document.querySelector("#add-todo-container");
 const editContainer = document.querySelector("#edit-todo-container");
 const editInput = document.querySelectorAll("input")[1];
-
-let targetIndex = 0;
+let targetIndex;
 
 function editTodo(index) {
   if (isEditing) {
-    displayError()
-  } 
-  else {
-    toggleInputDiv();
+    displayError();
+  } else {
     editInput.value = allTodos[index];
     targetIndex = index;
+    toggleInputDiv();
   }
 }
 
 function updateTodo() {
-  toggleInputDiv();
   allTodos.splice(targetIndex, 1, editInput.value);
+  saveTodos()
   printAllTodos();
+  toggleInputDiv();
 }
 
 let isEditing = false;
 
 function toggleInputDiv() {
-  if (isEditing) {
-    addContainer.className = "";
-    editContainer.className += " hide";
-  } else {
+  if (!isEditing) {
     editContainer.className = "";
     addContainer.className += " hide";
+  } else {
+    editContainer.className += " hide";
+    addContainer.className = "";
   }
+
   isEditing = !isEditing;
 }
 
@@ -69,9 +81,13 @@ const error = document.querySelector(".error")
 
 function displayError(){
    error.innerHTML = `
-     <p style="color: red;">Please save the current todo first</p>
+      <h3 class="red">Please save the current todo first</h3>
    `
    setTimeout(() => {
       error.innerHTML = ""
-   }, 3000)
+   }, 1300  )
+}
+
+function saveTodos(){
+  localStorage.setItem("todos", JSON.stringify(allTodos))
 }
